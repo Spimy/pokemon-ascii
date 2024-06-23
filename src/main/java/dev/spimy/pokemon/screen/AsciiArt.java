da7289f6d206765b9d1951bf1811234527ced744
Spimy <officialspyme@gmail.com>
2024-06-23 15:29:37 +0800

    👍 IMPROVE: Fix indentations

--- src/main/java/dev/spimy/pokemon/screen/AsciiArt.java
+++ src/main/java/dev/spimy/pokemon/screen/AsciiArt.java
@@ -1,88 +1,87 @@
 package dev.spimy.pokemon.screen;
 
 
-import java.util.ArrayList;
 import org.jline.jansi.Ansi.Color;
 import org.jline.terminal.Terminal;
 
+import java.util.ArrayList;
+
 public class AsciiArt {
-  final private ArrayList<Color> logoTheme;
-  final private ArrayList<Color> controlsTheme;
-  final private ArrayList<Color> gameOverTheme;
-  final private ArrayList<Color> pauseTheme;
+    final private ArrayList<Color> logoTheme;
+    final private ArrayList<Color> controlsTheme;
+    final private ArrayList<Color> gameOverTheme;
+    final private ArrayList<Color> pauseTheme;
 
-
-
-  public AsciiArt(Terminal terminal, Theme theme)  {
-    logoTheme = theme.getTheme();
-    controlsTheme = theme.getTheme();
-    gameOverTheme = theme.getTheme();
-    pauseTheme = theme.getTheme();
-  }
+    public AsciiArt(Terminal terminal, Theme theme) {
+        logoTheme = theme.getTheme();
+        controlsTheme = theme.getTheme();
+        gameOverTheme = theme.getTheme();
+        pauseTheme = theme.getTheme();
+    }
 
     public String getLogo() {
         return """
-              ╔───────────────────────────────────────────────────────────────╗
-              │██████╗  ██████╗ ██╗  ██╗███████╗███╗   ███╗ ██████╗ ███╗   ██╗│
-              │██╔══██╗██╔═══██╗██║ ██╔╝██╔════╝████╗ ████║██╔═══██╗████╗  ██║│
-              │██████╔╝██║   ██║█████╔╝ █████╗  ██╔████╔██║██║   ██║██╔██╗ ██║│
-              │██╔═══╝ ██║   ██║██╔═██╗ ██╔══╝  ██║╚██╔╝██║██║   ██║██║╚██╗██║│
-              │██║     ╚██████╔╝██║  ██╗███████╗██║ ╚═╝ ██║╚██████╔╝██║ ╚████║│
-              │╚═╝      ╚═════╝ ╚═╝  ╚═╝╚══════╝╚═╝     ╚═╝ ╚═════╝ ╚═╝  ╚═══╝│
-              ╚───────────────────────────────────────────────────────────────╝
-              """;
-  }
+                ╔───────────────────────────────────────────────────────────────╗
+                │██████╗  ██████╗ ██╗  ██╗███████╗███╗   ███╗ ██████╗ ███╗   ██╗│
+                │██╔══██╗██╔═══██╗██║ ██╔╝██╔════╝████╗ ████║██╔═══██╗████╗  ██║│
+                │██████╔╝██║   ██║█████╔╝ █████╗  ██╔████╔██║██║   ██║██╔██╗ ██║│
+                │██╔═══╝ ██║   ██║██╔═██╗ ██╔══╝  ██║╚██╔╝██║██║   ██║██║╚██╗██║│
+                │██║     ╚██████╔╝██║  ██╗███████╗██║ ╚═╝ ██║╚██████╔╝██║ ╚████║│
+                │╚═╝      ╚═════╝ ╚═╝  ╚═╝╚══════╝╚═╝     ╚═╝ ╚═════╝ ╚═╝  ╚═══╝│
+                ╚───────────────────────────────────────────────────────────────╝
+                """;
+    }
 
-  public String getControls() {
-      return """
-            ▐▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▌
-            ▐      Controls        ▌
-            ▐  P - [ Play-Pause ]  ▌
-            ▐  W - [ Up ]          ▌
-            ▐  A - [ Left ]        ▌
-            ▐  S - [ Down ]        ▌
-            ▐  D - [ Right ]       ▌
-            ▐  Q - [ Quit ]        ▌
-            ▐▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▌
-            """;
-  }
+    public String getControls() {
+        return """
+                ▐▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▌
+                ▐      Controls        ▌
+                ▐  P - [ Play-Pause ]  ▌
+                ▐  W - [ Up ]          ▌
+                ▐  A - [ Left ]        ▌
+                ▐  S - [ Down ]        ▌
+                ▐  D - [ Right ]       ▌
+                ▐  Q - [ Quit ]        ▌
+                ▐▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▌
+                """;
+    }
 
-   public String getGameOver() {
-       return """
-             ▐▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▌
-             ▐                                            ▌
-             ▐    ░█▀▀░█▀█░█▄█░█▀▀░░░█▀█░█░█░█▀▀░█▀▄░█    ▌
-             ▐    ░█░█░█▀█░█░█░█▀▀░░░█░█░▀▄▀░█▀▀░█▀▄░▀    ▌
-             ▐    ░▀▀▀░▀░▀░▀░▀░▀▀▀░░░▀▀▀░░▀░░▀▀▀░▀░▀░▀    ▌
-             ▐                                            ▌
-             ▐▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▌
-             """;
-  }
-  
-   public String getPause() {
-       return """
-             ┌──────────────────┐
-             │╔═╗╔═╗╦ ╦╔═╗╔═╗╔╦╗│
-             │╠═╝╠═╣║ ║╚═╗║╣  ║║│
-             │╩  ╩ ╩╚═╝╚═╝╚═╝═╩╝│
-             └──────────────────┘
-             """;
-  }
+    public String getGameOver() {
+        return """
+                ▐▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▌
+                ▐                                            ▌
+                ▐    ░█▀▀░█▀█░█▄█░█▀▀░░░█▀█░█░█░█▀▀░█▀▄░█    ▌
+                ▐    ░█░█░█▀█░█░█░█▀▀░░░█░█░▀▄▀░█▀▀░█▀▄░▀    ▌
+                ▐    ░▀▀▀░▀░▀░▀░▀░▀▀▀░░░▀▀▀░░▀░░▀▀▀░▀░▀░▀    ▌
+                ▐                                            ▌
+                ▐▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▌
+                """;
+    }
 
-  public ArrayList<Color> getLogoTheme() {
-    return logoTheme;
-  }
+    public String getPause() {
+        return """
+                ┌──────────────────┐
+                │╔═╗╔═╗╦ ╦╔═╗╔═╗╔╦╗│
+                │╠═╝╠═╣║ ║╚═╗║╣  ║║│
+                │╩  ╩ ╩╚═╝╚═╝╚═╝═╩╝│
+                └──────────────────┘
+                """;
+    }
 
-  public ArrayList<Color> getControlsTheme() {
-    return controlsTheme;
-  }
+    public ArrayList<Color> getLogoTheme() {
+        return logoTheme;
+    }
 
-  public ArrayList<Color> getGameOverTheme() {
-    return gameOverTheme;
-  }
+    public ArrayList<Color> getControlsTheme() {
+        return controlsTheme;
+    }
 
-  public ArrayList<Color> getPauseTheme() {
-    return pauseTheme;
-  }
+    public ArrayList<Color> getGameOverTheme() {
+        return gameOverTheme;
+    }
+
+    public ArrayList<Color> getPauseTheme() {
+        return pauseTheme;
+    }
 
 }
