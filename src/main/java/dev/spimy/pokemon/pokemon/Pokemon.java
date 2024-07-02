50b010592ed032c69e53e2b2cd902c1eec62233c
Spimy <officialspyme@gmail.com>
2024-07-03 01:33:22 +0800

    👍 IMPROVE: Add crit rate, exp and level stats

--- src/main/java/dev/spimy/pokemon/pokemon/Pokemon.java
+++ src/main/java/dev/spimy/pokemon/pokemon/Pokemon.java
@@ -2,29 +2,43 @@ package dev.spimy.pokemon.pokemon;
 
 public class Pokemon {
 
-    // Name of the Pokemon
+    // Name of the Pokémon
     private String name;
 
-    // Type of the Pokemon (e.g., fire, water, grass)
+    // Type of the Pokémon (e.g., fire, water, grass)
     private PokemonType type;
 
-    // Max HP (hit points/health) of the Pokemon
-    private double maxHp;
+    // Max HP (hit points/health) of the Pokémon
+    private int maxHp;
 
-    // Current HP (hit points/health) of the Pokemon
-    private double currentHp;
+    // Current HP (hit points/health) of the Pokémon
+    private int currentHp;
 
-    // Attack power of the Pokemon
+    // Attack power of the Pokémon
     private double attackPower;
 
-    // Speed of the Pokemon
+    // Speed of the Pokémon
     private double speed;
 
-    // Current status of the Pokemon (e.g., healthy, poisoned, paralyzed)
+    // Current status of the Pokémon (e.g., healthy, poisoned, paralyzed)
     private String status;
 
+    private float critRate;
+
+    private int exp;
+
     // Default constructor
-    public Pokemon(final String name, final PokemonType type, final double maxHp, final double currentHp, final double attackPower, final double speed, final String status) {
+    public Pokemon(
+            final String name,
+            final PokemonType type,
+            final int maxHp,
+            final int currentHp,
+            final double attackPower,
+            final double speed,
+            final String status,
+            final float critRate,
+            final int exp
+    ) {
         this.name = name;
         this.type = type;
         this.maxHp = maxHp;
@@ -32,26 +46,32 @@ public class Pokemon {
         this.attackPower = attackPower;
         this.speed = speed;
         this.status = status;
+        this.critRate = critRate;
+        this.exp = exp;
     }
 
     // Getters and Setters
+    public String getName() {
+        return this.name;
+    }
+
     public PokemonType getType() {
         return type;
     }
 
-    public double getMaxHp() {
+    public int getMaxHp() {
         return maxHp;
     }
 
-    public void setMaxHp(final double maxHp) {
+    public void setMaxHp(final int maxHp) {
         this.maxHp = maxHp;
     }
 
-    public double getCurrentHp() {
+    public int getCurrentHp() {
         return currentHp;
     }
 
-    public void setCurrentHp(final double currentHp) {
+    public void setCurrentHp(final int currentHp) {
         this.currentHp = currentHp;
     }
 
@@ -79,5 +99,34 @@ public class Pokemon {
         this.status = status;
     }
 
+    public float getCritRate() {
+        return critRate;
+    }
+
+    public void setCritRate(float critRate) {
+        this.critRate = critRate;
+    }
+
+    public int getExp() {
+        return exp;
+    }
+
+    public void setExp(int exp) {
+        this.exp = exp;
+    }
+
+    /**
+     * Level is scaled exponentially based on the exp of the Pokémon.
+     * For each level, the amount of exp required to level up again is 1.5x the previous amount of exp.
+     *
+     * @return level of the Pokémon
+     */
+    public int getLevel() {
+        final int baseExp = 500;
+        final double growthRate = 1.5;
+
+        final double level = Math.floor((Math.log((double) this.exp / baseExp) / Math.log(growthRate)) + 1);
+        return (int) level;
+    }
 }
 
