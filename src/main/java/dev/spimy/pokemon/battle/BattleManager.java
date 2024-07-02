46f0f731e590a03147a07296f1e89411766e683f
Spimy <officialspyme@gmail.com>
2024-07-03 01:34:33 +0800

    👍 IMPROVE: Calculate damage based on Pokémon stats

--- src/main/java/dev/spimy/pokemon/battle/BattleManager.java
+++ src/main/java/dev/spimy/pokemon/battle/BattleManager.java
@@ -4,17 +4,25 @@ import dev.spimy.pokemon.GameManager;
 import dev.spimy.pokemon.State;
 import dev.spimy.pokemon.battle.qte.ActionSelection;
 import dev.spimy.pokemon.battle.qte.AttackAction;
+import dev.spimy.pokemon.pokemon.Pokemon;
+import dev.spimy.pokemon.pokemon.PokemonType;
 
 import java.util.*;
 
 public class BattleManager {
 
     // TODO: Switch int array to pokemon array, int for now is pokemon HP stat
-    private final Integer[] opponents = new Integer[]{100, 100};
-    private final Integer[] playerPokemons = new Integer[]{100, 100};
+    private final Pokemon[] opponents = new Pokemon[]{
+            new Pokemon("Squirtle", PokemonType.WATER, 100, 100, 100, 100, "", 75, 200),
+            new Pokemon("Charmander", PokemonType.FIRE, 100, 100, 100, 100, "", 75, 200)
+    };
+    private final Pokemon[] playerPokemons = new Pokemon[]{
+            new Pokemon("Squirtle", PokemonType.WATER, 100, 100, 100, 100, "", 75, 200),
+            new Pokemon("Charmander", PokemonType.FIRE, 100, 100, 100, 100, "", 75, 200)
+    };
 
     // Stores the index of caught Pokémon where the index is the same as the one for array of opponents
-    private final List<Integer> caughtPokemons = new ArrayList<>();
+    private final List<Pokemon> caughtPokemons = new ArrayList<>();
 
     private final GameManager gameManager;
 
@@ -28,18 +36,34 @@ public class BattleManager {
 
     private void battleLoop() {
         while (this.gameManager.getState() == State.BATTLE) {
-            System.out.printf("Opponent 1 HP: %s/100%s%n", this.opponents[0], this.caughtPokemons.contains(0) ? "(caught)" : "");
-            System.out.printf("Opponent 2 HP: %s/100%s%n", this.opponents[1], this.caughtPokemons.contains(1) ? "(caught)" : "");
-            System.out.printf("Player Pokemon 1 HP: %s/100%n", this.playerPokemons[0]);
-            System.out.printf("Player Pokemon 2 HP: %s/100%n", this.playerPokemons[1]);
+            System.out.printf(
+                    "Opponent 1 HP: %s/%s%s%n",
+                    this.opponents[0].getCurrentHp(),
+                    this.opponents[0].getMaxHp(),
+                    this.caughtPokemons
+                            .stream()
+                            .anyMatch(o -> o.getName().equals(this.opponents[0].getName())) ? "(caught)" : ""
+            );
+            System.out.printf("Opponent 2 HP: %s/%s%s%n",
+                    this.opponents[1].getCurrentHp(),
+                    this.opponents[1].getMaxHp(),
+                    this.caughtPokemons
+                            .stream()
+                            .anyMatch(o -> o.getName().equals(this.opponents[1].getName())) ? "(caught)" : ""
+            );
+            System.out.printf("Player Pokemon 1 HP: %s/100%n", this.playerPokemons[0].getCurrentHp());
+            System.out.printf("Player Pokemon 2 HP: %s/100%n", this.playerPokemons[1].getCurrentHp());
 
-            final boolean caughtAndDeadPokemon = Arrays.stream(this.opponents).anyMatch(o -> o == 0) && !this.caughtPokemons.isEmpty();
-            final boolean allOpponentPokemonDead = Arrays.stream(this.opponents).allMatch(o -> o == 0);
-            final boolean allPlayerPokemonDead = Arrays.stream(this.playerPokemons).allMatch(o -> o == 0);
+            final boolean caughtAndDeadPokemon = Arrays.stream(this.opponents)
+                    .anyMatch(o -> o.getCurrentHp() == 0) && !this.caughtPokemons.isEmpty();
+            final boolean allOpponentPokemonDead = Arrays.stream(this.opponents)
+                    .allMatch(o -> o.getCurrentHp() == 0);
+            final boolean allPokemonCaught = this.caughtPokemons.size() >= this.opponents.length;
+            final boolean allPlayerPokemonDead = Arrays.stream(this.playerPokemons)
+                    .allMatch(o -> o.getCurrentHp() == 0);
 
             // If all opponents Pokémon have 0 HP or if all player Pokémon have 0 HP, the battle is over
-            final boolean isBattleOver = caughtAndDeadPokemon || allOpponentPokemonDead || allPlayerPokemonDead;
-            if (isBattleOver) {
+            if (caughtAndDeadPokemon || allOpponentPokemonDead || allPokemonCaught || allPlayerPokemonDead) {
                 // TODO: Calculate battle points
 
                 if (allPlayerPokemonDead) {
@@ -53,77 +77,126 @@ public class BattleManager {
             }
 
             // If HP is below 50 then the Pokémon can be caught
-            final Optional<Integer> catchablePokemon = Arrays.stream(this.opponents).filter(o -> o > 0 && o < 50).findFirst();
+            final Optional<Pokemon> catchablePokemon = Arrays.stream(this.opponents)
+                    .filter(o -> o.getCurrentHp() > 0 && o.getCurrentHp() < 50)
+                    .findFirst();
 
             if (catchablePokemon.isEmpty()) {
                 this.battle();
                 continue;
             }
 
-            // TODO: start QTE to choose between attack
-            // This simulates the result of the QTE action for choosing attack or attempt catch
             final boolean isBattle = new ActionSelection(this.gameManager, this)
                     .execute(5)
                     .isBattle();
 
             if (isBattle) {
                 System.out.println("battle");
-                System.out.println(
-                    new AttackAction(this.gameManager, this)
-                            .execute(2)
-                            .getCharged()
-                );
                 this.battle();
                 continue;
             }
 
             System.out.println("catch");
-            this.catchPokemon(Arrays.asList(this.opponents).indexOf(catchablePokemon.get()));
+            this.catchPokemon(catchablePokemon.get());
         }
     }
 
+    /**
+     *
+     */
     private void battle() {
-        // TODO: randomly choose QTE action
-        // Currently just does a random amount of damage
+        final Pokemon opponent = this.choosePokemon(this.opponents);
+        final Pokemon playerPokemon = this.choosePokemon(this.playerPokemons);
+
+        boolean attemptDodge = false;
+        final int opponentIndex = opponent.getSpeed() > playerPokemon.getSpeed() ? 0 : 1;
+
+        for (int i = 0; i < 2; i++) {
+            if (i == opponentIndex) {
+//                attemptDodge = true;
+                final Random random = new Random();
+                final int charge = random.nextInt(1, 100);
+                this.attack(opponent, playerPokemon, charge);
+            } else {
+                if (attemptDodge) return;
+                final AttackAction action = new AttackAction(this.gameManager, this).execute(2);
+                final int charge = action.getCharged();
+                this.attack(playerPokemon, opponent, charge);
+            }
+        }
+    }
+
+    private Pokemon choosePokemon(final Pokemon[] pokemonList) {
         final Random random = new Random();
 
-        // The following damage calculation will be based on Pokémon speed
-
-        // ---- DAMAGE CALCULATION FOR OPPONENT POKEMON ----
         int target = -1;
-        while (target == -1 || this.opponents[target] == 0 || this.caughtPokemons.contains(target)) {
-            target = random.nextInt(0, this.opponents.length);
+        boolean isCaught = true;
+
+        // If the selected Pokémon has fainted or was caught, select again
+        while (target == -1 || pokemonList[target].getCurrentHp() == 0 || isCaught) {
+            target = random.nextInt(0, pokemonList.length);
+            isCaught = this.caughtPokemons.contains(pokemonList[target]);
         }
 
-        // TODO: calculate damage based on player and opponent Pokémon type
-        this.opponents[target] -= random.nextInt(0, this.opponents[target] + 1);
-
-        // If all opponents are dead then player does not need to take damage
-        if (Arrays.stream(this.opponents).allMatch(o -> o == 0)) return;
-
-        // ---- DAMAGE CALCULATION FOR PLAYER POKEMON ----
-        int playerTarget = random.nextInt(0, this.playerPokemons.length);
-        while (playerTarget == -1 || this.playerPokemons[playerTarget] == 0 || this.caughtPokemons.contains(playerTarget)) {
-            playerTarget = random.nextInt(0, this.playerPokemons.length);
-        }
-
-        // TODO: calculate damage based on player and opponent Pokémon type
-        this.playerPokemons[playerTarget] -= random.nextInt(0, this.playerPokemons[playerTarget] + 1);
+        return pokemonList[target];
     }
 
+    /**
+     * @param from   the attacking Pokémon
+     * @param to     the target Pokémon
+     * @param charge the amount the attacking Pokémon has charged up
+     */
+    private void attack(final Pokemon from, final Pokemon to, final int charge) {
+        final int newHp = to.getCurrentHp() - this.getDamage(from, to, charge);
+        to.setCurrentHp(Math.max(newHp, 0));
+    }
+
+    /**
+     * A variation of the damage formula used by the classic Gen 1 Pokémon game.
+     * <a href="https://m.bulbapedia.bulbagarden.net/wiki/Damage">Source</a>.
+     *
+     * @param from   the attacking Pokémon
+     * @param to     the target Pokémon
+     * @param charge the amount the attacking Pokémon has charged up
+     * @return the amount of damage dealt
+     */
+    private int getDamage(final Pokemon from, final Pokemon to, final int charge) {
+        final int extraCritThreshold = 80;
+        final int extraCritRate = charge >= extraCritThreshold ? 15 : 0;
+        final boolean isCrit = this.gameManager.getSuccessChance(from.getCritRate() + extraCritRate);
+
+        final int critical = isCrit ? 2 : 1;
+        final int constant1 = 2;
+        final int constant2 = 5;
+
+        final double typeMultiplier;
+        if (to.getType().getWeakAgainst().contains(from.getType())) {
+            typeMultiplier = 1.5;
+        } else if (from.getType().getWeakAgainst().contains(to.getType())) {
+            typeMultiplier = 0.5;
+        } else {
+            typeMultiplier = 1;
+        }
+
+        final double part1 = ((double) (constant1 * from.getLevel() * critical) / constant2) + constant1;
+        final double part2 = ((part1 * from.getAttackPower()) / (constant2 * 10)) + constant1;
+        final double damage = part2 * typeMultiplier * new Random().nextInt(1, 10);
+
+        return (int) damage;
+    }
 
     /**
      * Kept simple, not following the exact formula for catching.
      * Success chance rates:
      * - Poké Ball: 15%
-     * - Great Ball: 22.5% (1.5x)
      * - Ultra Ball: 30% (2x)
+     * - Master Ball: 100%
      *
-     * @param pokemonIndex the index of the opponent Pokémon in the opponents array
+     * @param pokemon the index of the opponent Pokémon in the opponents array
      */
-    private void catchPokemon(final int pokemonIndex) {
+    private void catchPokemon(final Pokemon pokemon) {
         final boolean success = this.gameManager.getSuccessChance(10);
         if (!success) return;
-        this.caughtPokemons.add(pokemonIndex);
+        this.caughtPokemons.add(pokemon);
     }
 }
