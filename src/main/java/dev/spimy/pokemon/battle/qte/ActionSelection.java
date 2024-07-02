1bff7d98a5ecf4a942adb97853b8f2f25b529da9
Spimy <officialspyme@gmail.com>
2024-07-02 18:25:36 +0800

    📦 NEW: Add action selection qte

---
+++ src/main/java/dev/spimy/pokemon/battle/qte/ActionSelection.java
@@ -0,0 +1,44 @@
+package dev.spimy.pokemon.battle.qte;
+
+import dev.spimy.pokemon.GameManager;
+import dev.spimy.pokemon.battle.BattleManager;
+
+public class ActionSelection extends QuickTimeEvent<ActionSelection> {
+    private boolean isBattle;
+
+    public ActionSelection(GameManager gameManager, BattleManager battleManager) {
+        super(gameManager, battleManager);
+    }
+
+    /**
+     * @param eventTimeSeconds the amount of time this QTE can last for in seconds
+     * @return the action selection instance
+     */
+    @Override
+    public ActionSelection execute(final int eventTimeSeconds) {
+        System.out.println("W: Battle | S: Catch");
+
+        while (System.currentTimeMillis() < this.startTime + (eventTimeSeconds * 1000L)) {
+            if (this.gameManager.getControl().isUp(qteActionKey)) {
+                this.qteActive = false;
+                this.isBattle = true;
+            }
+
+            if (this.gameManager.getControl().isDown(qteActionKey)) {
+                this.qteActive = false;
+                this.isBattle = false;
+            }
+
+            if (this.qteActive) continue;
+            return this;
+        }
+
+        this.qteActive = false;
+        this.isBattle = true;
+        return this;
+    }
+
+    public boolean isBattle() {
+        return this.isBattle;
+    }
+}
