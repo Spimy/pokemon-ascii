3c693fbf0289d76c53a92b997e4c5b4cd9a7a5c6
Spimy <officialspyme@gmail.com>
2024-07-03 02:17:15 +0800

    👍 IMPROVE: Remove unused code

--- src/main/java/dev/spimy/pokemon/battle/qte/QuickTimeEvent.java
+++ src/main/java/dev/spimy/pokemon/battle/qte/QuickTimeEvent.java
@@ -26,20 +26,6 @@ public abstract class QuickTimeEvent<T> {
     protected boolean qteActive = true;
     protected volatile int qteActionKey; // Thank you, Lai, for teaching me about the volatile keyword
 
-    private enum QTEType {
-        SELECT_ACTION(5),
-        CATCH_POKEMON(5),
-        CHARGE_UP_ATTACK(10),
-        DODGE_ATTACK(30);
-
-        // In seconds
-        public final int eventTime;
-
-        QTEType(final int eventTime) {
-            this.eventTime = eventTime;
-        }
-    }
-
     public QuickTimeEvent(final GameManager gameManager, final BattleManager battleManager) {
         this.random = new Random();
         this.gameManager = gameManager;
@@ -55,43 +41,5 @@ public abstract class QuickTimeEvent<T> {
             return;
         }
         this.qteActionKey = key;
-
-//        switch (currentQTE) {
-//            case QTEType.CATCH_POKEMON:
-//                if (this.gameManager.getControl().isEnter(key)) {
-//                    qteActive = false;
-//                    int result = random.nextInt(100) + 1;
-//                    if (result <= 15) {
-//                        System.out.println("You got a Poké Ball!");
-//                    } else if (result <= 45) {
-//                        System.out.println("You got an Ultra Ball!");
-//                    } else {
-//                        System.out.println("You got a Master Ball!");
-//                    }
-//                }
-//                break;
-//
-//            case QTEType.CHARGE_UP_ATTACK:
-//                System.out.println("Tap 'W' Repeatedly to Charge Up");
-//                if (this.gameManager.getControl().isUp(key)) {
-//                    pressCount++;
-//                    if (pressCount >= 20) {
-//                        qteActive = false;
-//                        System.out.println("Attack fully charged! Critical hit!"); // Perform critical hit (1.5x damage)
-//                    }
-//                }
-//                break;
-//
-//            case QTEType.DODGE_ATTACK:
-//                System.out.println("A: LEFT | D: RIGHT");
-//                if (this.gameManager.getControl().isLeft(key)) {
-//                    qteActive = false;
-//                    System.out.println("Dodged left!"); // Perform left dodge
-//                } else if (this.gameManager.getControl().isRight(key)) {
-//                    qteActive = false;
-//                    System.out.println("Dodged right!"); // Perform right dodge
-//                }
-//                break;
-//        }
     }
 }
