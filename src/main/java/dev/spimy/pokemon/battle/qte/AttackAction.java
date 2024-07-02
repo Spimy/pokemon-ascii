9848831b78101a255dd591f4cb69c8aa65157965
Spimy <officialspyme@gmail.com>
2024-07-03 01:37:47 +0800

    👍 IMPROVE: Convert max charge to local variable

--- src/main/java/dev/spimy/pokemon/battle/qte/AttackAction.java
+++ src/main/java/dev/spimy/pokemon/battle/qte/AttackAction.java
@@ -5,7 +5,6 @@ import dev.spimy.pokemon.battle.BattleManager;
 
 public class AttackAction extends QuickTimeEvent<AttackAction> {
     private int charged;
-    private final int maxCharge = 100;
 
     public AttackAction(final GameManager gameManager, final BattleManager battleManager) {
         super(gameManager, battleManager);
@@ -14,14 +13,15 @@ public class AttackAction extends QuickTimeEvent<AttackAction> {
     @Override
     public AttackAction execute(final int eventTimeSeconds) {
         System.out.println("Tap 'W' Repeatedly to Charge Up");
+        final int maxCharge = 100;
 
         while (System.currentTimeMillis() < this.startTime + (eventTimeSeconds * 1000L)) {
             if (this.gameManager.getControl().isUp(this.qteActionKey)) {
                 final int amount = this.random.nextInt(1, 10);
 
                 System.out.printf("%s -> ", this.charged);
-                if (this.charged + amount >= this.maxCharge) {
-                    this.charged = this.maxCharge;
+                if (this.charged + amount >= maxCharge) {
+                    this.charged = maxCharge;
                     this.qteActive = false;
                     System.out.println(this.charged);
                     return this;
@@ -39,9 +39,5 @@ public class AttackAction extends QuickTimeEvent<AttackAction> {
 
     public int getCharged() {
         return this.charged;
-    }
-
-    public int getMaxCharge() {
-        return this.maxCharge;
     }
 }
