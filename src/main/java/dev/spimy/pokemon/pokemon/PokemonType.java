06d6c960bfa53a6da544c610aac2fbea2f7078fc
Spimy <officialspyme@gmail.com>
2024-07-12 01:22:25 +0800

    📦 NEW: Add more pokemon types

--- src/main/java/dev/spimy/pokemon/pokemon/PokemonType.java
+++ src/main/java/dev/spimy/pokemon/pokemon/PokemonType.java
@@ -5,14 +5,32 @@ import java.util.List;
 public enum PokemonType {
 	FIRE,
 	WATER,
-	GRASS;
-	
+	GRASS,
+	ELECTRIC,
+	NORMAL,
+	BUG,
+	FAIRY,
+	POISON,
+	PSYCHIC,
+	ROCK,
+	GHOST,
+	STEEL;
+
 	private List<PokemonType> weakAgainst;
 	
 	static {
-		FIRE.weakAgainst = List.of(WATER);
-		WATER.weakAgainst = List.of(GRASS);
-		GRASS.weakAgainst = List.of(FIRE);
+		FIRE.weakAgainst = List.of(WATER, ROCK);
+		WATER.weakAgainst = List.of(GRASS, ELECTRIC);
+		GRASS.weakAgainst = List.of(FIRE, POISON, BUG);
+		ELECTRIC.weakAgainst = List.of();
+		NORMAL.weakAgainst = List.of();
+		BUG.weakAgainst = List.of(FIRE, ROCK);
+		FAIRY.weakAgainst = List.of(POISON, STEEL);
+		POISON.weakAgainst = List.of(PSYCHIC);
+		PSYCHIC.weakAgainst = List.of(BUG, GHOST);
+		ROCK.weakAgainst = List.of(WATER, GRASS, STEEL);
+		GHOST.weakAgainst = List.of(GHOST);
+		STEEL.weakAgainst = List.of(FIRE, GHOST);
 	}
 	
 	public List<PokemonType> getWeakAgainst(){
